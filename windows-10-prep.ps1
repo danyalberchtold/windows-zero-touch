b713b0e66900efbe5d7ff4fe8b2710c474afbfbd
Danyal Berchtold <danyal.berchtold@ict.csbe.ch>
2024-06-04 14:59:30 +0200

    windows-10-prep.ps1

---
+++ windows-10-prep.ps1
@@ -0,0 +1,11 @@
+# Delete the bootfix.bin
+Remove-Item (Join-Path $FolderPath "boot\bootfix.bin") -Force
+
+# Rename the efisys files
+Rename-Item (Join-Path $FolderPath "efi\microsoft\boot\efisys.bin") "efisys_prompt.bin" 
+Rename-Item (Join-Path $FolderPath "efi\microsoft\boot\efisys_noprompt.bin") "efisys.bin" 
+
+# Rename install.esd to install.wim
+If (Test-Path $(Join-Path $FolderPath "source\install.esd")){
+	Rename-Item $(Join-Path $FolderPath "source\install.esd") "install.wim"
+}
